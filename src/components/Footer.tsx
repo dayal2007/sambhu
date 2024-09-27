56bfdb280a6b425993cb357912c18e03c022b935
Sambhu Dayal Bahuleyan <dayal2007@hotmail.com>
2024-09-27 22:32:59 +0100

    Responsive fixes for homepage styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ const Footer = () => {
             <footer id="footer" className="footer position-relative">
             <div className="container">
             <div className="copyright text-center ">
+                <p className='big'><i className ="bi bi-code-square"></i> This website is built using React combined with TypeScript</p>
                 <p>© <span>Copyright</span> <strong className="px-1 sitename">Sambhu.ie</strong> <span>All Rights Reserved</span></p>
             </div>
             <div className="credits">
